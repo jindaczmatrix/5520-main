09ff432b92bc19f539c262d952f9b002842f492a
unlimited <osx@pop-os.localdomain>
2023-07-03 16:52:39 -0700

    var

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = "cool-app.js"
   return (
     <View style={styles.container}>
       <Text>Test📔</Text>
